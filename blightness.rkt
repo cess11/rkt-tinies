228cf2a3962a740f4ec2c1762d1bcce3b051bb3f
PNS11 <pontus@pontusnaslund.se>
2022-11-30 16:24:48 +0100

    Major refactoring for greater evil, also comments

--- blightness.rkt
+++ blightness.rkt
@@ -1,38 +1,68 @@
-#lang racket
+#lang racket/base
 
 (require racket/gui/easy
          racket/gui/easy/operator)
 
+; configure screen brightness file, this one works with my toshiba laptop
 (define @brightness-file (@ "/sys/class/backlight/intel_backlight/brightness"))
 
+; read and return first line from screen brightness file,
+; converted from string to number
 (define (load-brightness file)
   (call-with-input-file file
     (lambda (in)
       (string->number (read-line in)))))
 
+; observable holding the current screen brightness
 (define @bright (@ (load-brightness (obs-peek @brightness-file))))
 
-(define (sub500 val)
-  (- val 500))
-
-(define (add500 val)
-  (+ val 500))
-
-(define (update-brightness brightness action)
+; write to the screen brightness file, takes a
+; string representing a number, e.g. "1500"
+(define (write-brightness numstring)
   (call-with-output-file (obs-peek @brightness-file)
     (lambda (out)
-      (display (number->string brightness) out))
-    #:exists 'truncate)
-  (obs-update! @bright action))
+      (display numstring out))
+    #:exists 'truncate))
 
+; subtract 500 from val if bigger than 550
+; at 0 screen is black, hence the +50
+(define (sub500 val)
+  (if (> val 550)
+      (- val 500)
+      500))
+
+; add 500 to val if less than 4000
+(define (add500 val)
+  (if (< val 4000)
+      (+ val 500)
+      4000))
+
+; update @bright and write to the screen brightness file,
+; takes a function that works with obs-update!
+(define (update-brightness! action)
+  (obs-update! @bright action)
+  (write-brightness (number->string (obs-peek @bright))))
+
+; write arbitrary value to screen brightness file
+(define (set-brightness! value)
+  (write-brightness (number->string value)))
 
 (define (app)
   (window
    #:title "Blightness Adjustor Robot"
-   (hpanel
-    (button "-" (lambda () (update-brightness (- (obs-peek @bright) 500) sub500)))
-    (text (@bright . ~> . number->string))
-    (button "+" (lambda () (update-brightness (+ (obs-peek @bright) 500) add500))))))
+   (vpanel
+    (hpanel
+     (button "-" (lambda () (update-brightness! sub500)))
+     (button "+" (lambda () (update-brightness! add500)))
+     (button "500" (lambda () (update-brightness! (lambda (ev) 500))))
+     (button "1500" (lambda () (update-brightness! (lambda (ev) 1500))))
+     (button "3500" (lambda () (update-brightness! (lambda (ev) 3500)))))
+    (vpanel
+     (slider
+      #:min-value 0
+      #:max-value 5000
+      @bright (lambda (val) (and (set-brightness! val) val)) ; (λ:= @bright)
+      #:style '(horizontal plain))))))
 
 (module+ main
   (render (app)))
